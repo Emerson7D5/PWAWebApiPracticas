a9f6f857f8f56d9ad1abd5f6fc5c3a94cb2bdb99
JoseAguilarTA <josea@techaffinity.com>
2023-02-25 12:57:29 -0600

    Add project files.

---
+++ P01WebApi/Models/equipos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01WebApi.Models
+{
+    public class equipos
+    {
+        [Key]
+        public int id_equipos { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int? tipo_equipo_id { get; set; }
+        public int? marca_id { get; set; }
+    }
+}
